39854d724115bccc0925dca56db2832211fe9a06
Guilherme_Amorim <cezarguilherme03@gmail.com>
2021-10-01 17:13:08 -0300

    integração com WEB

---
+++ MVCRazorCRUD/Controllers/AlunoController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MVCRazorCRUD.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCRazorCRUD.Controllers
+{
+    //o controller recebe as requisições WEB
+    [Route ("Aluno")]
+
+    public class AlunoController : Controller 
+    {
+        //criando o método para receber uma ação
+        //e encaminhar para a view
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route ("Cadastrar")]
+
+        public IActionResult Cadastrar(IFormCollection formulario)
+        {
+            Aluno aluno = new Aluno();
+
+            //vamos receber os dados do formulário
+            aluno.Nome = formulario["alunoNome"];
+            aluno.Email = formulario["alunoEmail"];
+            aluno.Endereco = formulario["alunoEnd"];
+            aluno.Telefone = formulario["alunoTel"];
+            aluno.Escolaridade = formulario["alunoEsc"];
+
+            aluno.CadastroAluno(aluno);
+
+            return LocalRedirect("/");
+        }
+        [Route("Cadastro")]
+        public IActionResult Cadastro()
+        {    
+            return View();
+        }
+    }
+}
---
+++ MVCRazorCRUD/Models/Aluno.cs
@@ -0,0 +1,68 @@
+﻿using MVCRazorCRUD.Context;
+using MVCRazorCRUD.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCRazorCRUD.Models
+{
+    public class Aluno : UsuarioBase , IAluno
+    { 
+        public string Escolaridade { get; set; }
+
+        public Aluno AlterarAluno(Aluno aluno)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Aluno CadastroAluno(Aluno aluno)
+        {
+            try
+            {
+                //defininfo a conexão -- precisamos criar a classe de conexao
+                var connectioin = Conexao.GetConnect();
+                connectioin.Open();
+                var query = "insert into Alunos (alunoNome, alunoEmail, alunoEndereco, alunoTelefone, alunoEscolaridade) values (@nome, @email,@end,@tel,@esc)";
+                
+                //juntando a query com a conexao
+                var command = new SqlCommand(query, connectioin);
+
+                //agora vamos atribuir os valores para as variaveis - Formato CamelCase
+                command.Parameters.Add("@nome", SqlDbType.VarChar).Value = aluno.Nome;
+                command.Parameters.Add("@email", SqlDbType.VarChar).Value = aluno.Email;
+                command.Parameters.Add("@end", SqlDbType.VarChar).Value = aluno.Endereco;
+                command.Parameters.Add("@tel", SqlDbType.VarChar).Value = aluno.Telefone;
+                command.Parameters.Add("@esc", SqlDbType.VarChar).Value = aluno.Escolaridade;
+
+                //executando a query
+                command.ExecuteNonQuery();
+                
+                //fechamos a conexao
+                connectioin.Close();
+                
+                //retornamos o aluno cadastrado
+                return aluno;
+
+
+            }
+            catch (Exception )
+            {
+
+                throw;
+            }
+        }
+
+        public List<Aluno> ListarAluno()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Aluno RemoverAluno(int Id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
